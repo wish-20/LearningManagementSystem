fe7a25810633ee3af6679b18ea227f61288c9682
umniyah <119628318+wish-20@users.noreply.github.com>
2023-10-28 21:16:09 +0300

    SystemDbContext.cs

--- SystemDbContext.cs
+++ SystemDbContext.cs
@@ -5,7 +5,31 @@ using System.Web;
 
 namespace LMS.Data
 {
-    public class SystemDbContext
+    public class SystemDbContext:DbContext
     {
+        public SystemDbContext(DbContextOptions<SystemDbContext> options) : base(options) 
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            //modelBuilder.Entity<>()
+        }
+        public DbSet<Course>courses {  get; set; }
+        public DbSet<Instructer> Instructer { get; set; }
+
+        public DbSet<Progress> Progress { get; set; }
+        public DbSet<Enrollment> Enrollment { get; set; }
+        public DbSet<Student> Student { get; set; }
+        public DbSet<Video> Video { get; set; }
+        public DbSet<Module> Module { get; set; }
+        public DbSet<Review> Review { get; set; }
+
+
+
+
+
     }
 }
